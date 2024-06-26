15bfb326c994ac0aeeaee177ebf61361ebbbb28b
JustMouse <Just.Mouse@yandex.ru>
2024-06-26 10:16:28 +0300

    Create gtag.js

---
+++ gtag.js
@@ -0,0 +1,7 @@
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-CTQFQQMY65"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-CTQFQQMY65');
+</script>
